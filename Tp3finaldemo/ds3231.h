1d2d6bd106fc92e8f5b5fb1ffec8d0bb9b82f30f
benja287 <rodriguezmantilla123@gmail.com>
2024-06-24 19:32:31 -0300

    se arreglo lo de registrar datos poniendo twit init

--- Tp3finaldemo/ds3231.h
+++ Tp3finaldemo/ds3231.h
@@ -1,15 +1,4 @@
-/*
-#ifndef DS3231_H_
-#define DS3231_H_
 
-#include <avr/io.h>
-
-void DS3231_GetTime(uint8_t *second, uint8_t *minute, uint8_t *hour, uint8_t *day, uint8_t *date, uint8_t *month, uint8_t *year);
-void DS3231_SetTime(uint8_t second, uint8_t minute, uint8_t hour, uint8_t day, uint8_t date, uint8_t month, uint8_t year);
-uint8_t get_time(char* buffer, int tamanio);
-
-
-#endif // DS3231_H_ */
 
 #ifndef DS3231_H_
 #define DS3231_H_
--- Tp3finaldemo/mef.h
+++ Tp3finaldemo/mef.h
@@ -1,22 +1,4 @@
 
-/*
-#ifndef MEF_H_
-#define MEF_H_
-
-// Función para inicializar la MEF
-void Datos_Init(void);
-
-// Función para actualizar el estado actual basado en la entrada y el estado actual
-void Actualizar_Datos(void);
-
-#endif // MEF_H_ */
-/*
- * mef.h
- *
- * Created: 10/6/2024 18:37:39
- *  Author: Alexis
- */ 
-
 #ifndef MEF_H_
 #define MEF_H_
 
